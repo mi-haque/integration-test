80f4da788c5960e6a451e2e5b2155832dd6d3731
mohammed <mohammed.haque@news.co.uk>
2017-04-03 13:44:48 +0530

    change in echo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 // * sample program
 public class HelloWorld {
     public static void main(String[] args) { 
-        System.out.println("Integrated");
+        System.out.println("Integrated successfully");
     }
 }
